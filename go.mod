6c0f8ff199d68576829316b0bcb36493a59b18eb
Christian Scherer <christian.scherer@deutsche-boerse.de>
2021-04-15 16:15:17 +0200

    New

---
+++ go.mod
@@ -0,0 +1,5 @@
+module MediaManagement
+
+go 1.14
+
+require github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
---
+++ pkg/MediaUtils/MetaReader.go
@@ -0,0 +1,36 @@
+package MediaUtils
+
+import (
+	"MediaManagement/pkg/Setup"
+	"github.com/rwcarlsen/goexif/exif"
+	"log"
+	"os"
+)
+
+func ReadVideoMeta(fname string, fileStr *Setup.FileStruct) {
+	readFromFile(fname, fileStr)
+}
+
+func ReadPhotoMeta(fname string, fileStr *Setup.FileStruct) {
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Seek(0, 0)
+	x, err := exif.Decode(f)
+	if err != nil {
+		readFromFile(fname, fileStr)
+	} else {
+		fileStr.CreationDate, _ = x.DateTime()
+		fileStr.MetaOrigin = Setup.METAORIGINMETA
+	}
+}
+
+func readFromFile(fname string, fileStr *Setup.FileStruct) {
+	fileStat, err := os.Stat(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileStr.CreationDate = fileStat.ModTime()
+	fileStr.MetaOrigin = Setup.METAORIGINFILE
+}
